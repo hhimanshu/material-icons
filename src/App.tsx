27803d23235025eb8fb8bc1bb306feffcea21080
hhimanshu <harit.himanshu@yahoo.in>
2021-07-14 06:52:45 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MdAccountCircle } from "react-icons/md";
+import { MdFace } from "react-icons/md";
+import { MdVerifiedUser } from "react-icons/md";
+import { MdPlayCircleFilled } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          All Icons from react-icons
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="icons-container">
+          <MdAccountCircle size={100}/>
+          <MdFace size={100}/>
+          <MdVerifiedUser size={100}/>
+          <MdPlayCircleFilled size={100}/>
+        </div>
       </header>
     </div>
   );
